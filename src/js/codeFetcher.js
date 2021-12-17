6500d81dc7a024e4e4dcf4604175abef22f1c2a5
Jonas Holbech <jh@jonasholbech.dk>
2021-12-17 12:33:22 +0100

    build server working

--- src/js/codeFetcher.js
+++ src/js/codeFetcher.js
@@ -1,5 +1,4 @@
 export default async function codeFetcher(folder, data) {
-  console.log(data);
   let returnObj = {};
   await Promise.all(
     data.map(async (path) => {
@@ -9,6 +8,5 @@ export default async function codeFetcher(folder, data) {
       returnObj[path] = await resp.text();
     })
   );
-  console.log(returnObj);
   return returnObj;
 }
