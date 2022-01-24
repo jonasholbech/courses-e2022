908162f5c6dc7e5434b22bcde8661833f3243f6e
Jonas Holbech <jh@jonasholbech.dk>
2022-01-24 13:12:41 +0100

    create pdf's from links

---
+++ scanFolders.js
@@ -0,0 +1,33 @@
+const { readdirSync } = require("fs");
+const { exec } = require("child_process");
+
+const getDirectories = (source) =>
+  readdirSync(source, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => source + "/" + dirent.name);
+
+let urlsToParse = [];
+const topLevelDirectories = getDirectories("./src/pages");
+topLevelDirectories.forEach((url) => {
+  urlsToParse = urlsToParse.concat(getDirectories(url));
+});
+console.log(urlsToParse);
+urlsToParse.forEach((url) => {
+  const parts = url.split("/");
+  const file = parts.at(-1);
+  const folder = parts.at(-2);
+  exec(
+    `npm run pdf -- http://localhost:3000/${folder}/${file} ./pdfs/${folder}/${file}.pdf`,
+    (error, stdout, stderr) => {
+      if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+    }
+  );
+});
