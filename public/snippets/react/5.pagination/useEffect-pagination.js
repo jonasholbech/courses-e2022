4974ee195f816de3ee57b5258227f694c33dc1cf
Jonas Holbech <jh@jonasholbech.dk>
2022-01-25 10:04:47 +0100

    react/5.pagination done

---
+++ public/snippets/react/5.pagination/useEffect-pagination.js
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+export default function App() {
+  const [page, setPage] = useState(0);
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://kea-alt-del.dk/t7/api/products?start=${page * 10}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setArticles((prev) => prev.concat(data));
+        setLoading(false);
+      });
+  }, [page]);
+
+  return (
+    <>
+      <ol>
+        {articles.map((art) => (
+          <li key={art.id}>{art.name}</li>
+        ))}
+      </ol>
+      {loading && <p>loading more data</p>}
+      <button onClick={() => setPage((prev) => prev + 1)}>Load more</button>
+    </>
+  );
+}
