8e4f2c4a357a0f33ce835046c79f372f68e3a0ee
Jonas Holbech <jh@jonasholbech.dk>
2022-02-03 23:21:09 +0100

    agendasections in t7/5

--- public/snippets/t7/5.fetch-arrays-and-foreach/fetches6.js
+++
@@ -1,7 +0,0 @@
-fetch("https://somesite.com/api/...")
-  .then((e) => e.json())
-  .then(buildIt);
-
-function buildIt(data) {
-  console.log(data);
-}
--- public/snippets/t7/5.fetch-arrays-and-foreach/full-fetch.js
+++ public/snippets/t7/5.fetch-arrays-and-foreach/full-fetch.js
@@ -1,15 +1,17 @@
 fetch("SOME-API-ENDPOINT")
-  .then(response => {
+  .then((response) => {
     if (!response.ok) {
       throw Error(response.statusText);
     }
     return response.json();
   })
-  .then(data => {
-    //We have the data
-    console.log(data);
-  })
-  .catch(e => {
+  .then(dataReceived)
+  .catch((e) => {
     //Woops, something went wrong
     console.error("An error occured:", e.message);
   });
+
+function dataReceived(data) {
+  //We have the data
+  console.log(data);
+}
