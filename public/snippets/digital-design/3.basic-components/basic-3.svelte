ff09d899ee732e4abeb1728dc1d9a63489ba0cef
Jonas Holbech <jh@jonasholbech.dk>
2022-02-22 09:31:51 +0100

    digi/3.basic-components

---
+++ public/snippets/digital-design/3.basic-components/basic-3.svelte
@@ -0,0 +1,11 @@
+<script>
+  //this is where we write our scripts
+  //import other components etc
+</script>
+
+<article class="feature">
+  <h2>Markup</h2>
+  <p>Any HTML goes here</p>
+</article>
+
+<style></style>
---
+++ public/snippets/digital-design/3.basic-components/basic-4.svelte
@@ -0,0 +1,18 @@
+<script>
+  //this is where we write our scripts
+  //import other components etc
+</script>
+
+<article class="feature">
+  <h2>Markup</h2>
+  <p>Any HTML goes here</p>
+</article>
+
+<style>
+  /* "Scoped styling" 
+All CSS written here will only "work" 
+for this component */
+  article {
+    border: 1px solid #000;
+  }
+</style>
