b8dd7a5bc990f95d673c0f4291e7eeff0611198b
Jonas Holbech <jh@jonasholbech.dk>
2022-03-09 09:49:38 +0100

    digi/5

--- public/snippets/digital-design/5.javascript/EachTutorial.svelte
+++ public/snippets/digital-design/5.javascript/EachTutorial.svelte
@@ -9,10 +9,10 @@
 <h1>The Famous Cats of YouTube</h1>
 
 <ul>
-  {#each cats as cat, i}
+  {#each cats as cat}
     <li>
       <a target="_blank" href="https://www.youtube.com/watch?v={cat.id}">
-        {i + 1}: {cat.name}
+        {cat.name}
       </a>
     </li>
   {/each}
